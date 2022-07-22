244ab2b8eac6d825aea1400059ba2f89e8c13cd7
PERCY99 <43267172+PERCY99@users.noreply.github.com>
2022-07-22 16:58:37 +0530

    Update Form.js

--- first/src/components/Form.js
+++ first/src/components/Form.js
@@ -1,17 +1,31 @@
 import React from 'react'
-
+import { useState } from 'react'
 function Form() {
+    const [name, setName] = useState("");
+    const [pass, setPass] = useState("")
+    
+    const handleChange = (e) =>{
+        e.preventDefault();
+        console.log(name , pass);
+    }
     return (
         <div>
-            <form>
+            <form onSubmit={handleChange}>
                 <label>Name
-                    <input type="text" name="name"/>
+                    <input 
+                        type="text" 
+                        name="name"
+                        value={name}
+                        onChange ={(e) => setName(e.target.value)} />
                 </label>
-                <br></br>
                 <label>Password
-                    <input type="password" name="password"/>
+                    <input 
+                        type="password" 
+                        name="password"
+                        value={pass}
+                        onChange = {(e) => setPass(e.target.value)}/>
                 </label>
-                
+                <input type="submit"/>
             </form>
         </div>
     )
